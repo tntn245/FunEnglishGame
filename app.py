2cf4f746b77989cdc98f620544c022a1edb5c551
tntn245 <96983352+tntn245@users.noreply.github.com>
2022-09-18 22:06:50 +0700

    update

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from operator import truediv
 from flask import Flask, render_template, request, url_for, redirect
 from pymongo import MongoClient
 from bson.objectid import ObjectId
@@ -7,8 +8,6 @@ from datetime import datetime
 app = Flask(__name__)
 
 client = MongoClient('mongodb://localhost:27017/')
-# c = MongoClient(103.116.106.153:2718)
-# app.config["MONGO_URI"] = "mongodb://localhost:27017/feg"
 test = MongoClient('mongodb://root:123@103.116.106.153:2718/?authMechanism=DEFAULT')
 
 
@@ -29,20 +28,24 @@ def index():
     if request.method=='POST':
         sentences = request.form['sentences']
         new_dialog = request.form['new_dialog']
-        feg.insert_one({'sentences': sentences, 'dialog': new_dialog})
+        new = request.form['new']
+        feg.insert_one({'sentences': sentences, 'dialog': new_dialog, 'new': new})
         return redirect(url_for('index'))
 
     all_feg = list(feg.find())
     all_todos = list(todos.find())
+    all_user = list(user.find())
     all_Dialog = list(Dialog.find())
     all_Dialog_reverse = reversed(all_Dialog)
     all_Users = list(Users.find())
     all_EnglishSentence = list(EnglishSentence.find())
     all_DialogContentNotification = list(DialogContentNotification.find())
-    
-    return render_template('index.html', feg=all_feg, todos=all_todos, 
+
+    dialog_find=feg.find().sort("Date", -1).limit(1)
+
+    return render_template('index.html', feg=all_feg, todos=all_todos, user=all_user,
     Dialog=all_Dialog, Users=all_Users, EnglishSentence=all_EnglishSentence, DialogContentNotification=all_DialogContentNotification,
-    Dialog_reverse=all_Dialog_reverse)
+    Dialog_reverse=all_Dialog_reverse, New_Dialog=dialog_find)
 
 @app.route('/new_dialog', methods=('GET', 'POST'))
 def new_dialog():
@@ -55,24 +58,29 @@ def new_dialog():
     all_feg = list(feg.find())
     return render_template('index.html', feg=all_feg)
 
-# @app.route('/dialog', methods=('GET', 'POST'))
-# def dialog():
-    # if request.method=='POST':
-    #     dialog = request.form['dialog']
-    #     feg.insert_one({'dialog': new_dialog})
-    #     return redirect(url_for('index'))
-
-    # all_feg = list(feg.find())
-    # return render_template('index.html', feg=all_feg)
-
-@app.route('/sentences', methods=('GET', 'POST'))
-def sentences():
+@app.route('/<id>/sentences/', methods=('GET', 'POST'))
+def sentences(id):
     if request.method=='POST':
         sentences = request.form['sentences']
-        dialog=feg.find().sort("Date", -1)
-        sentence={'ReceiverName': '', 'Sentence': sentences, 'Seen': '', 'TimeStamp': '', 'DialogId':dialog[0]._id}
-        feg.insert_one(sentence)
+        res = feg.aggregate([{'$sample': {'size': 1 }}])
+        randomName =list(res)
+        sentence={'ReceiverName': randomName[0]['Name'], 'Sentence': sentences, 'Seen': False, 'TimeStamp': '', 'Sent': False, 'CreatedBy': '', 'CreatedDate': datetime.now(), 'ModifiedBy':'', 'ModifiedDate': datetime.now(), 'DialogId':ObjectId(id)}
+        todos.insert_one(sentence)
 
+        return redirect(url_for('index'))
+
+    all_feg = list(feg.find())
+    return render_template('index.html', feg=all_feg)
+
+@app.route('/replay_dialog', methods=('GET', 'POST'))
+def replay_dialog():
+    if request.method=='POST':
+        replay_dialog = request.form['replay_dialog']
+        for i in sentences:
+            res = feg.aggregate([{'$sample': {'size': 1 }}])
+            randomName =list(res)
+            sentence={'ReceiverName': randomName[0]['Name'], 'Sentence': i['Sentence'], 'Seen': False, 'TimeStamp': '', 'Sent': False, 'CreatedBy': '', 'CreatedDate': datetime.now(), 'ModifiedBy':'', 'ModifiedDate': datetime.now(), 'DialogId':ObjectId(replay_dialog)}
+            todos.insert_one(sentence)
         return redirect(url_for('index'))
 
     all_feg = list(feg.find())
@@ -85,11 +93,16 @@ def delete(id):
 
 @app.post('/<id>/get_row/')
 def get_row(id):
-    feg.delete_one({"_id": ObjectId(id)})
+    todos.update_one({"_id": ObjectId(id)},{"$set":{"Sent":True}})
     return redirect(url_for('index'))
 
-@app.post('/<id>/find/')
-def find(id):
-    feg.delete_one({"_id": ObjectId(id)})
+@app.post('/send_all_dialog')
+def send_all_dialog():
+    todos.update_many({"Sent": False},{"$set":{"Sent":True}})
+    return redirect(url_for('index'))
+
+@app.post('/<id>/disable/')
+def disable(id):
+    user.update_one({"_id": ObjectId(id)},{"$set":{"Online":False}})
     return redirect(url_for('index'))
 
